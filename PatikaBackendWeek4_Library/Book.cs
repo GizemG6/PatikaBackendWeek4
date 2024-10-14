5621966e60b543460761938cf1183b105a93dca9
cang8 <0.gizemgunes@gmail.com>
2024-10-14 20:08:28 +0300

    added-patikaLibrary

---
+++ PatikaBackendWeek4_Library/Book.cs
@@ -0,0 +1,32 @@
+﻿public class Book
+{
+    // Properties - These define the characteristics of a Book object
+    public string Title { get; set; }
+    public string AuthorFirstName { get; set; }
+    public string AuthorLastName { get; set; }
+    public int PageCount { get; set; }
+    public string Publisher { get; set; }
+    public DateTime RegistrationDate { get; set; }
+
+    public Book() // Default Constructor - This is called when an object is created with no parameters
+    {
+        RegistrationDate = DateTime.Now; // Automatically sets the registration date to the current time
+        Console.WriteLine("Book object created!");
+    }
+
+    // Parameterized Constructor - This constructor assigns values during object creation
+    public Book(string title, string authorFirstName, string authorLastName, int pageCount, string publisher)
+    {
+        // Assigning the parameter values to the properties
+        Title = title;
+        AuthorFirstName = authorFirstName;
+        AuthorLastName = authorLastName;
+        PageCount = pageCount;
+        Publisher = publisher;
+        RegistrationDate = DateTime.Now;
+    }
+    public void DisplayBookInfo()
+    {
+        Console.WriteLine($"Title: {Title}, Author: {AuthorFirstName} {AuthorLastName}, Pages: {PageCount}, Publisher: {Publisher}, Registration Date: {RegistrationDate}");
+    }
+}
---
+++ PatikaBackendWeek4_Library/Program.cs
@@ -0,0 +1,10 @@
+﻿Book book1 = new Book(); // The 'new' keyword creates an instance of the Book class
+book1.Title = "Adı Aylin";
+book1.AuthorFirstName = "Ayşe";
+book1.AuthorLastName = "Kulin";
+book1.PageCount = 398;
+book1.Publisher = "Remzi Kitabevi";
+book1.DisplayBookInfo();
+
+Book book2 = new Book("Adı Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi"); // 'new' keyword creates a new instance, while constructor assigns initial values
+book2.DisplayBookInfo();
