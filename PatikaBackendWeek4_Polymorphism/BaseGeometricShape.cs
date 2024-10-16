bc450e2da17ac8a7fb224e47bc03ca5482e17096
cang8 <0.gizemgunes@gmail.com>
2024-10-16 14:45:41 +0300

    added-polymorphism

---
+++ PatikaBackendWeek4_Polymorphism/BaseGeometricShape.cs
@@ -0,0 +1,32 @@
+﻿public class BaseGeometricShape // base class for geometric shapes
+{
+    // properties
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public virtual double CalculateArea() // virtual method for calculating area
+    {
+        return Height * Width;
+    }
+}
+
+public class Square : BaseGeometricShape // derived class for square
+{
+    public override double CalculateArea() // override method for square area 
+    {
+        return Width * Width; // both width ang height are the same
+    }
+}
+
+public class Rectangle : BaseGeometricShape // derived class for rectangle
+{
+    // no need to override
+}
+
+public class RightTriangle : BaseGeometricShape // derived class for right triangle
+{
+    public override double CalculateArea()
+    {
+        return (Width * Height) / 2; // override method for right triangle
+    }
+}
---
+++ PatikaBackendWeek4_Polymorphism/Program.cs
@@ -0,0 +1,16 @@
+﻿// create a square
+Square square = new Square();
+square.Width = 5.1;
+Console.WriteLine($"Square area :{square.CalculateArea()}");
+
+// create a rectangle
+Rectangle rectangle = new Rectangle();
+rectangle.Width = 6.3;
+rectangle.Height = 4.3;
+Console.WriteLine($"Rectangle area :{rectangle.CalculateArea()}");
+
+//create a right triangle
+RightTriangle rightTriangle = new RightTriangle();
+rightTriangle.Width = 7.2;
+rightTriangle.Height = 2.9;
+Console.WriteLine($"Right triangle area :{rightTriangle.CalculateArea()}");
